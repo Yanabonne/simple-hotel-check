7f2ccde92762fe1cf2bc30ef503b70bbb595232b
Yana Morozova <yanabonne@yandex.ru>
2023-03-23 13:07:20 +0300

    feat: add scrollbar element

--- src/shared/Scrollbar/Scrollbar.js
+++ src/shared/Scrollbar/Scrollbar.js
@@ -73,22 +73,16 @@ function Scrollbar({ listRef }) {
       const { current: trackCurrent } = scrollTrackRef;
       const { current: contentCurrent } = listRef;
       if (trackCurrent && contentCurrent) {
-        // First, figure out where we clicked
         const { clientY } = e;
-        // Next, figure out the distance between the top of the track and the top of the viewport
         const target = e.target;
         const rect = target.getBoundingClientRect();
         const trackTop = rect.top;
-        // We want the middle of the thumb to jump to where we clicked, so we subtract half the thumb's height to offset the position
         const thumbOffset = -(thumbHeight / 2);
-        // Find the ratio of the new position to the total content length using the thumb and track values...
         const clickRatio =
           (clientY - trackTop + thumbOffset) / trackCurrent.clientHeight;
-        // ...so that you can compute where the content should scroll to.
         const scrollAmount = Math.floor(
           clickRatio * contentCurrent.scrollHeight
         );
-        // And finally, scroll to the new position!
         contentCurrent.scrollTo({
           top: scrollAmount,
           behavior: "smooth",
@@ -107,8 +101,6 @@ function Scrollbar({ listRef }) {
           scrollHeight: contentScrollHeight,
           offsetHeight: contentOffsetHeight,
         } = listRef.current;
-
-        // Subtract the current mouse y position from where you started to get the pixel difference in mouse position. Multiply by ratio of visible content height to thumb height to scale up the difference for content scrolling.
         const deltaY =
           (e.clientY - scrollStartPosition) *
           (contentOffsetHeight / thumbHeight);
@@ -123,7 +115,6 @@ function Scrollbar({ listRef }) {
     [isDragging, scrollStartPosition, thumbHeight]
   );
 
-  // Listen for mouse events to handle scrolling by dragging the thumb
   React.useEffect(() => {
     document.addEventListener("mousemove", handleThumbMousemove);
     document.addEventListener("mouseup", handleThumbMouseup);
@@ -157,50 +148,3 @@ function Scrollbar({ listRef }) {
 }
 
 export default Scrollbar;
-
-const Scrollba = () => {
-  const contentRef = React.useRef(null);
-  const scrollTrackRef = React.useRef(null);
-  const scrollThumbRef = React.useRef(null);
-  const observer = React.useRef(null);
-  const [thumbHeight, setThumbHeight] = React.useState(20);
-
-  function handleResize(ref, trackSize) {
-    const { clientHeight, scrollHeight } = ref;
-    setThumbHeight(Math.max((clientHeight / scrollHeight) * trackSize, 20));
-  }
-
-  React.useEffect(() => {
-    if (contentRef.current && scrollTrackRef.current) {
-      const ref = contentRef.current;
-      const { clientHeight: trackSize } = scrollTrackRef.current;
-      observer.current = new ResizeObserver(() => {
-        handleResize(ref, trackSize);
-      });
-      observer.current.observe(ref);
-      return () => {
-        observer.current?.unobserve(ref);
-      };
-    }
-  }, []);
-
-  return (
-    <div className="custom-scrollbars__container">
-      <div className="custom-scrollbars__content" ref={contentRef}></div>
-      <div className="custom-scrollbars__scrollbar">
-        <button className="custom-scrollbars__button">⇑</button>
-        <div className="custom-scrollbars__track-and-thumb">
-          <div className="custom-scrollbars__track"></div>
-          <div
-            className="custom-scrollbars__thumb"
-            ref={scrollThumbRef}
-            style={{
-              height: `${thumbHeight}px`,
-            }}
-          ></div>
-        </div>
-        <button className="custom-scrollbars__button">⇓</button>
-      </div>
-    </div>
-  );
-};
